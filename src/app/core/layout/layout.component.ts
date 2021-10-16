e10da0176d4d90dcc861b3036ccfb6b83d6ac9b2
oles.kor@2c2p.com <oles.kor@2c2p.com>
2021-10-16 15:34:26 +0300

    Added header animation

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -2,10 +2,26 @@ import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material/sidenav';
 import { delay } from 'rxjs/operators';
+import { trigger, style, animate, transition } from '@angular/animations';
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  animations: [
+    trigger(
+      'headerAnimation', [
+        transition(':enter', [
+          style({height: 0, opacity: 0, padding: 0}),
+          animate('500ms', style({opacity: 1, height: '64px'}))
+        ]),
+        transition(':leave', [
+          style({opacity: 1, height: '64px'}),
+          animate('500ms', style({height: 0, opacity: 0, padding: 0}))
+        ])
+      ]
+    )
+  ],
 })
 export class LayoutComponent {
   @ViewChild(MatSidenav) sidenav!: MatSidenav;
