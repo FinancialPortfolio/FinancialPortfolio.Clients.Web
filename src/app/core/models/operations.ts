68af726af8e075c2715e93d669a5689c289aa470
OlesKorzhynskyy <oles.korzhynskiy@gmail.com>
2022-09-17 21:51:14 +0300

    Added integrations

--- src/app/core/models/operations.ts
+++ src/app/core/models/operations.ts
@@ -7,10 +7,12 @@ export const AccountUpdatedOperation = "AccountUpdatedEvent";
 export const AccountDeletedOperation = "AccountDeletedEvent";
 
 export const TransferCreatedOperation = "TransferCreatedEvent";
+export const TransfersIntegratedOperation = "TransfersIntegratedEvent";
 export const TransferUpdatedOperation = "TransferUpdatedEvent";
 export const TransferDeletedOperation = "TransferDeletedEvent";
 
 export const OrderCreatedOperation = "OrderCreatedEvent";
+export const OrdersIntegratedOperation = "OrdersIntegratedEvent";
 export const OrderUpdatedOperation = "OrderUpdatedEvent";
 export const OrderDeletedOperation = "OrderDeletedEvent";
 
@@ -23,10 +25,12 @@ export const UpdateAccountOperation = "UpdateAccountCommand";
 export const DeleteAccountOperation = "DeleteAccountCommand";
 
 export const CreateTransferOperation = "CreateTransferCommand";
+export const IntegrateTransfersOperation = "IntegrateTransfersCommand";
 export const UpdateTransferOperation = "UpdateTransferCommand";
 export const DeleteTransferOperation = "DeleteTransferCommand";
 
 export const CreateOrderOperation = "CreateOrderCommand";
+export const IntegrateOrdersOperation = "IntegrateOrdersCommand";
 export const UpdateOrderOperation = "UpdateOrderCommand";
 export const DeleteOrderOperation = "DeleteOrderCommand";
 
@@ -40,10 +44,12 @@ export function getSuccessfulMessage(operation: SuccessfulOperation): string | n
         case AccountDeletedOperation: return "Account was deleted successfully.";
 
         case TransferCreatedOperation: return "Transfer was created successfully.";
+        case TransfersIntegratedOperation: return "Transfers were integrated successfully.";
         case TransferUpdatedOperation: return "Transfer was updated successfully.";
         case TransferDeletedOperation: return "Transfer was deleted successfully.";
 
         case OrderCreatedOperation: return "Order was created successfully.";
+        case OrdersIntegratedOperation: return "Orders were integrated successfully.";
         case OrderUpdatedOperation: return "Order was updated successfully.";
         case OrderDeletedOperation: return "Order was deleted successfully.";
 
@@ -59,10 +65,12 @@ export function getFailedMessage(operation: FailedOperation): string {
         case DeleteAccountOperation: return "Account wasn't deleted.";
 
         case CreateTransferOperation: return "Transfer wasn't created.";
+        case IntegrateTransfersOperation: return "Transfers weren't integrated.";
         case UpdateTransferOperation: return "Transfer wasn't updated.";
         case DeleteTransferOperation: return "Transfer wasn't deleted.";
 
         case CreateOrderOperation: return "Order wasn't created.";
+        case IntegrateOrdersOperation: return "Orders weren't integrated.";
         case UpdateOrderOperation: return "Order wasn't updated.";
         case DeleteOrderOperation: return "Order wasn't deleted.";
 
